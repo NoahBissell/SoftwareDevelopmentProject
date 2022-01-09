a3b3163857ba4e10ecac4153c5299db8b7df427e
Noah Bissell <s015470@students.lmsd.org>
2022-01-09 12:06:26 -0500

    Initial Commit

---
+++ SoftwareDevelopmentProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoftwareDevelopmentProject
+//
+//  Created by Noah Bissell (student LM) on 1/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoftwareDevelopmentProject/SoftwareDevelopmentProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareDevelopmentProjectApp.swift
+//  SoftwareDevelopmentProject
+//
+//  Created by Noah Bissell (student LM) on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SoftwareDevelopmentProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
